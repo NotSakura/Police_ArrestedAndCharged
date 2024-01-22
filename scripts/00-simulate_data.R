709c6f75b428286e31a18b5e214c55b9718155d1
Sakura Noskor <sakura.noskor@mail.utoronto.ca>
2024-01-22 20:55:35 +0000

    set the file structure and have set up a script that stimulates data but not test

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,20 @@
+#### Preamble ####
+# Purpose: Get data on 2021 shelter usage and make table
+# Author: Sakura Noskor
+# Email: sakura.noskor@mail.utoronto.ca
+# Date: 16 January 2024
+# Prerequisites: -
+
+#### Workspace setup ####
+#install.packages("opendatatoronto")
+#install.packages("janitor")
+#install.packages("lubridate")
+#install.packages("tidyverse")
+#install.packages("knitr")
+
+#### Simulate data ####
+library(knitr)
+library(janitor)
+library(lubridate)
+library(opendatatoronto)
+library(tidyverse)
